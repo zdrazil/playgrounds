bb675f48677f736dd3f22c9eb62b6721daa988f1
Vladimír Zdražil <mail@vzdrazil.com>
2020-09-12 12:16:49 +0200

    update some files

---
+++ js/src/lodash-fp.js
@@ -0,0 +1,23 @@
+const _ = require('lodash/fp');
+
+1 + 2;
+
+_.identity('a');
+
+5 * 8;
+
+var array = [
+  { dir: 'left', code: 97 },
+
+  { dir: 'right', code: 100 },
+];
+
+_.keyBy(array, function (o) {
+  return String.fromCharCode(o.code);
+});
+// => { 'a': { 'dir': 'left', 'code': 97 }, 'd': { 'dir': 'right', 'code': 100 } }
+
+_.keyBy(array, 'dir');
+// => { 'left': { 'dir': 'left', 'code': 97 }, 'right': { 'dir': 'right', 'code': 100 } }
+//
+//
